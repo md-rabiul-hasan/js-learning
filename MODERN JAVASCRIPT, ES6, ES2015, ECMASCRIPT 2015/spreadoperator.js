231e4e49f19219656691cb2e44ccf6169577371d
rabiulhasan3 <rabiul.fci@gmail.com>
2021-08-10 00:55:20 +0600

    spread operator

---
+++ MODERN JAVASCRIPT, ES6, ES2015, ECMASCRIPT 2015/spreadoperator.js
@@ -0,0 +1,24 @@
+const m = [1,2,3,4]
+const n = [5,6,7]
+const mn = m.concat(n);
+console.log(mn);
+
+let arr = [1,2,3];
+let arr2 = [4,5];
+  
+arr = [...arr,...arr2];
+console.log(arr); // [ 1, 2, 3, 4, 5 ]
+
+let a = [1,2,3]
+let b = [3,4]
+let c = [...a,5,...b]
+console.log(c) // [ 1, 2, 3, 5, 3, 4 ]
+
+
+const user1 = {
+    name: 'Jen',
+    age: 22
+};
+  
+const clonedUser = { ...user1 };
+console.log(clonedUser);
