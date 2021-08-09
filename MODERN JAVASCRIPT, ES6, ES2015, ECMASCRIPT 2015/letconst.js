a1d5ab5ed5fb1812f81740d67ecda81107b4f92d
rabiulhasan3 <rabiul.fci@gmail.com>
2021-08-10 00:08:06 +0600

    let-const

---
+++ MODERN JAVASCRIPT, ES6, ES2015, ECMASCRIPT 2015/letconst.js
@@ -0,0 +1,15 @@
+{
+    let yourname = "Arif";
+    console.log(yourname);
+}
+
+//console.log(yourname);  // variable not found .. it is scope operator
+
+const age = 23;
+// age =25; -- const variable data did not possible to change
+console.log(age);
+
+const friends = ['arif', 'munna', 'shuvo'];
+friends.push('farid');
+// friends = ['hasan', 'rabiul']; const array possilbe to data push pop.. but full data change not possible
+console.log(friends);
