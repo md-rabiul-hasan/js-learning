ceffe6467c3761023f7857bad49fb0e7dfe02a0d
rabiulhasan3 <rabiul.fci@gmail.com>
2021-08-10 00:13:39 +0600

    template

---
+++ MODERN JAVASCRIPT, ES6, ES2015, ECMASCRIPT 2015/template.js
@@ -0,0 +1,21 @@
+const firstname = "Rabiul";
+const lastname = "Hasan";
+
+const fullname = `My name is ${firstname} ${lastname}`
+console.log(fullname);
+
+const paragraph = `You leapt from crumbling bridges watching cityscapes turn to dust
+Filming helicopters crashing in the ocean from way above
+
+Got the music in you baby, tell me why
+Got the music in you baby, tell me why
+You've been locked in here forever and you just can't say goodbye
+
+Kisses on the foreheads of the lovers wrapped in your arms
+You've been hiding them in hollowed out pianos left in the dark
+
+Got the music in you baby, tell me why
+Got the music in you baby, tell me why
+You've been locked in here forever and you just can't say goodbye`
+console.log(paragraph)
+
